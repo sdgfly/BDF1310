dfeb86518e92e32a02f858d9399ad1fe8a86ff92
Phillip Moran <sdgfly@fullsaid.edu>
2013-10-11 22:14:47 -0400

    finished read
    added a body.inc file, created style sheet and styled page

--- MVCREAD/index.php
+++ MVCREAD/index.php
@@ -10,37 +10,8 @@ $views = new viewModel();
 //Show header here
 $views->getView("views/header.inc");
 
-$get_users = find_all_onions();
-?>
-<ul class="user_list">
-<?php
-if (isset($_GET["id"])) {
-	$selected_id = $_GET["id"];
-}else{
-	$selected_id = null;
-}
+$views->getView("views/body.inc");
 
-while ($row = mysqli_fetch_assoc($get_users)) {
-	echo "<li><a href=\"?id=" . $row['onionId'] . "\">" . $row['onionFirstName'] . " " . $row['onionLastName'] . "</a></li>";
-}
- 
-
-
- 
-?>
-
-</ul>
-
-<?php 
-
-if (isset($selected_id)) {
-	$current_onion = find_onion_by_id($selected_id);
-	echo "<p>Username: " . $current_onion["onionName"] . " " . $current_onion["onionFirstName"] ." " . $current_onion["onionLastName"] . " " . $current_onion["onionDOB"] . "</p>";
-}
-
-?>
-
-<?php
 //show footer
 $views->getView("views/footer.inc");
 
