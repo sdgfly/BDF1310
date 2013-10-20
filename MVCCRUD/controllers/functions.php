d0d4af5768bd356e82d56bdd2b5d12093dc66a87
Phillip Moran <sdgfly@fullsaid.edu>
2013-10-20 13:51:14 -0400

    Initial Creation
    Created the layout, changed the font for the header and footer, added
    @font-face and fonts folder with tinet.ttf file. Verified connection to
    database.

---
+++ MVCCRUD/controllers/functions.php
@@ -0,0 +1,65 @@
+<?php
+
+function confirm_query($result_set){
+
+	if (!$result_set) {
+		die("Database connection failed.");
+	}
+
+}
+
+function find_all_onions() {
+	
+	global $connection;
+
+	$query  = "SELECT * ";
+	$query .= "FROM onion ";
+	$query .= "ORDER BY onionName ASC";
+	$onion_set = mysqli_query($connection, $query);
+	confirm_query($onion_set);
+
+	return $onion_set;
+}
+
+function find_onion_by_id($onion_id){
+
+	global $connection;
+
+	$safe_onion_id = mysqli_real_escape_string($connection, $onion_id);
+
+	$query  = "SELECT * ";
+	$query .= "FROM onion ";
+	$query .= "WHERE onionId = {$safe_onion_id} ";
+	$query .= "LIMIT 1 ";
+	$onion_set = mysqli_query($connection, $query);
+	
+	confirm_query($onion_set);
+
+	if($onion = mysqli_fetch_assoc($onion_set)) {
+		return $onion;
+	}else{
+		return null;
+	}
+
+	
+
+
+}
+ 
+function find_one_onion($user) {
+
+	global $connection;
+
+	//$safe_onion = mysqli_real_escape_string($connection, $user);
+
+	$query = "SELECT * ";
+	$query .="FROM onion ";
+	$query .="WHERE onionId = {$user} ";
+
+	$onion_set = mysqli_query($connection, $query);
+	confirm_query($onion_set);
+
+	return $onion_set;
+}
+
+?>
---
+++ MVCCRUD/index.php
@@ -0,0 +1,22 @@
+<?php
+// index page
+require_once ('models/db_connection.php');
+require_once('controllers/functions.php');
+include ('models/viewModel.php');
+
+//make a new view
+$views = new viewModel();
+
+//Show header here
+$views->getView("views/header.inc");
+
+$views->getView("views/body.inc");   
+
+//show footer
+$views->getView("views/footer.inc");
+
+//find_one_onion($row['onionId']
+
+?>
+
+         
